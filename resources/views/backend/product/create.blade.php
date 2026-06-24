b60b507f0636be23b209c7a74ef02d9400babb47
Maha-bub <mdmahabubulalam0511@gmail.com>
2026-06-24 09:53:11 +0600

    include product create form field

--- resources/views/backend/product/create.blade.php
+++ resources/views/backend/product/create.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     <main class="page-content">
         <!--breadcrumb-->
+
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
             <div class="breadcrumb-title pe-3">Forms</div>
             <div class="ps-3">
@@ -23,7 +24,8 @@
                             href="javascript:;">Action</a>
                         <a class="dropdown-item" href="javascript:;">Another action</a>
                         <a class="dropdown-item" href="javascript:;">Something else here</a>
-                        <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:;">Separated link</a>
+                        <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:;">Separated
+                            link</a>
                     </div>
                 </div>
             </div>
@@ -37,20 +39,22 @@
                 <div class="card">
                     <div class="card-body p-4">
                         <h5 class="mb-4">Vertical Form</h5>
-                        <form class="row g-3">
+                        <form action="{{ route('product.store') }}" class="row g-3">
                             <div class="col-md-6">
                                 <label for="input1" class="form-label">Product Name</label>
                                 <input type="text" name="name" class="form-control" id="input1"
                                     placeholder="Product Name">
                             </div>
-                            <div class="col-md-6">
-                                <label for="input3" class="form-label">Category</label>
-                                <input type="text" name="category" class="form-control" id="input3"
-                                    placeholder="Description">
+                            <div class="col-6">
+                                <label for="AddCategory" class="form-label fw-bold">Category</label>
+                                <select class="form-select" id="AddCategory">
+                                    <option value="0">Topwear</option>
+                                </select>
                             </div>
-                            <div class="col-md-12">
-                                <label for="input3" class="form-label">Product Description</label>
-                                <input type="text" class="form-control" id="input3" placeholder="Description" rows="3">
+
+                            <div class="mb-4">
+                                <h5 class="mb-3">Product Description</h5>
+                                <textarea class="form-control" cols="4" rows="3" placeholder="write a description here.."></textarea>
                             </div>
                             <div class="col-md-6">
                                 <label for="input4" class="form-label">Price</label>
@@ -61,31 +65,27 @@
                                 <label class="form-label" for="Product_Status"><b>Product Status:</b></label> <br>
                                 <div class="mt-2">
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="stock" value="stock">
+                                        <input class="form-check-input" type="radio" name="stock" value="1">
                                         <label class="form-check-label" for="Stock">Stock</label>
                                     </div>
                                     <div class="form-check form-check-inline">
-                                        <input class="form-check-input" type="radio" name="stock" value="OutOfStock">
+                                        <input class="form-check-input" type="radio" name="stock" value="0">
                                         <label class="form-check-label" for="OutOfStock">Out Of Stock</label>
                                     </div>
 
                                     <div class="invalid-feedback">Product Status Is Required!</div>
                                 </div>
                             </div>
-                            
-                            
-                           
-                           
-                            <div class="col-md-12">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="input12">
-                                    <label class="form-check-label" for="input12">Check me out</label>
-                                </div>
+
+                            <div class="mb-4">
+                                <h5 class="mb-3">Choose images</h5>
+                                <input id="fancy-file-upload" type="file" name="files"
+                                    accept=".jpg, .png, image/jpeg, image/png" multiple>
                             </div>
                             <div class="col-md-12">
                                 <div class="d-md-flex d-grid align-items-center gap-3">
                                     <button type="button" class="btn btn-primary px-4">Submit</button>
-                                    <button type="button" class="btn btn-light px-4">Reset</button>
+
                                 </div>
                             </div>
                         </form>
@@ -93,6 +93,8 @@
                 </div>
             </div>
 
-        </div><!--end row-->
+        </div>
+
+
     </main>
 @endsection
